289aa9d815f7b6f05583825cb81af73e7bf82eea
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-06-19 21:17:25 -0300

    searching products in the api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react";
+import Header from "./components/Header";
+import Main from "./pages/main/index";
 
-import './styles.css';
+import "./styles.css";
 
 const App = () => (
     <div className="App">
       <Header />
+      <Main />
     </div>
 );
 
